d77bf3662228b3c393ea844594b0d1c00d02fc89
Terje Sylvarnes <terje@debian>
2020-04-13 17:11:01 +0200

    Add dependency to codox, for doc-generation.

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot game-of-life.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[lein-codox "0.10.7"]])
